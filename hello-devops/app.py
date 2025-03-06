cbe41f9bc5bd2b18c0c30f70da7fe3975dacbe51
ChadhaOuederni <chadha.ouederni1@gmail.com>
2025-03-06 14:31:58 +0100

    new feature1

--- hello-devops/app.py
+++ hello-devops/app.py
@@ -1,11 +1,11 @@
 # app .py
 from flask import Flask
-app = Flask ( __name__ )
-@app . route ('/')
+app = Flask( __name__ )
+@app.route ('/')
 def hello ():
  return "Hello , CI/CD!"
 @app.route('/status')
 def status():
     return "running"
 if __name__ == " __main__ ":
- app . run ()
+ app.run ()
--- hello-devops/test_app.py
+++ hello-devops/test_app.py
@@ -1,7 +1,7 @@
 import unittest
 from app import app
-class TestApp ( unittest . TestCase ):
+class TestApp ( unittest.TestCase ):
  def test_home_route ( self ):
-  tester = app . test_client ( self )
-  response = tester . get ('/')
-  self . assertEqual ( response . status_code , 200)
+  tester = app.test_client ( self )
+  response = tester.get ('/')
+  self.assertEqual ( response.status_code , 200)
